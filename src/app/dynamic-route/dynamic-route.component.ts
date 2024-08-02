58e20ddc993816af521b9657ac6e15f90c3791ef
lalit-shriffle <lalitp@shriffle.com>
2024-08-02 15:35:56 +0530

    created dynamic route and recieved params with ActivatedRoute

---
+++ src/app/dynamic-route/dynamic-route.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+@Component({
+  selector: 'app-dynamic-route',
+  standalone: true,
+  imports: [],
+  templateUrl: './dynamic-route.component.html',
+  styleUrl: './dynamic-route.component.css'
+})
+export class DynamicRouteComponent {
+  constructor(private route: ActivatedRoute) {
+      this.route.params.subscribe((params)=>{
+        console.log("params ",params["id"]);
+      })
+  }
+} 
