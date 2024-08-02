acc5cc344aedbff5a009900358b5fd977594aaa9
lalit-shriffle <lalitp@shriffle.com>
2024-08-02 13:15:37 +0530

    learned and created observables using rxjs of() operator

--- src/app/child1/child1.component.ts
+++ src/app/child1/child1.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { of } from 'rxjs';
 
 @Component({
   selector: 'app-child1',
@@ -8,5 +9,10 @@ import { Component } from '@angular/core';
   styleUrl: './child1.component.css'
 })
 export class Child1Component {
-
+  mYObservable = of(1, 2, 3, 4);
+  constructor() {
+  this.mYObservable.subscribe((data)=>{
+      console.log(data);
+    })
+  }
 }
