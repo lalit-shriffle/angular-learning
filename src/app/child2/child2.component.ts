92e7a862cf6e7c165b558681bd5ac1fa88fcfb51
lalit-shriffle <lalitp@shriffle.com>
2024-08-02 12:08:52 +0530

    applied dependency injection at component level

--- src/app/child2/child2.component.ts
+++ src/app/child2/child2.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
+import { AuthServiceService } from '../auth-service.service';
 
 @Component({
   selector: 'app-child2',
   standalone: true,
   imports: [],
   templateUrl: './child2.component.html',
-  styleUrl: './child2.component.css'
+  styleUrl: './child2.component.css',
+  providers: [AuthServiceService]
 })
 export class Child2Component {
-
+  constructor(authService: AuthServiceService){
+    const result:boolean = authService.loginUser("lalit","pass");
+    console.log("component level DI ",result);
+  }
 }
